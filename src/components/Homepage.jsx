1835b1356d3342d24b0e0f72db2b4025337c9d53
Samowusu <samuelowusu958@gmail.com>
2022-03-29 13:59:54 +0000

    my first commit

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import millify from "millify";
+import { Typography, Row, Col, Statistic } from "antd";
+import { Link } from "react-router-dom";
+
+import { useGetCryptosQuery } from "../services/cryptoApi";
+import { Cryptocurrencies, News } from "../components";
+
+function Homepage() {
+  const { data, isFetching } = useGetCryptosQuery(10);
+  const globalStats = data?.data?.stats;
+
+  if (isFetching) return "Loading...";
+
+  return (
+    <>
+      <Typography.Title level={2} className="heading">
+        Global Crypto Stats
+      </Typography.Title>
+      <Row>
+        <Col span={12}>
+          <Statistic
+            title="Total Cryptocurrencies"
+            value={millify(globalStats.total)}
+          />
+        </Col>
+        <Col span={12}>
+          <Statistic
+            title="Total Exchanges"
+            value={millify(globalStats.totalExchanges)}
+          />
+        </Col>
+        <Col span={12}>
+          <Statistic
+            title="Total Market Cap"
+            value={millify(globalStats.totalMarketCap)}
+          />
+        </Col>
+        <Col span={12}>
+          <Statistic
+            title="Total 24h Volume"
+            value={millify(globalStats.total24hVolume)}
+          />
+        </Col>
+        <Col span={12}>
+          <Statistic
+            title="Total Markets"
+            value={millify(globalStats.totalMarkets)}
+          />
+        </Col>
+      </Row>
+      <div className="home-heading-container">
+        <Typography.Title className="home-title" level={2}>
+          Top 10 Cryptocurrencies in the world
+        </Typography.Title>
+        <Typography.Title className="show-more" level={3}>
+          <Link to={"/cryptocurrencies"}>Show more</Link>
+        </Typography.Title>
+      </div>
+      <Cryptocurrencies simplified={true} />
+      <div className="home-heading-container">
+        <Typography.Title className="home-title" level={2}>
+          Latest Crypto News
+        </Typography.Title>
+        <Typography.Title className="show-more" level={3}>
+          <Link to={"/news"}>Show more</Link>
+        </Typography.Title>
+      </div>
+      <News />
+    </>
+  );
+}
+
+export default Homepage;
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+const cryptoApiHeaders = {
+  "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+  "X-RapidAPI-Key": "cca4e99a6dmshb956a508c338754p1f2f57jsn72b0bf155980",
+};
+
+const baseURL = "https://coinranking1.p.rapidapi.com";
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl: baseURL }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: (count) => createRequest(`/coins?limit=${count}`),
+    }),
+  }),
+});
+
+export const { useGetCryptosQuery } = cryptoApi;
